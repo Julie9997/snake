1d222eb88107e467a250ae4feb8025503b34edfe
Julie9997 <46169473+Julie9997@users.noreply.github.com>
2021-05-30 13:26:38 +0300

    Add files via upload
    snake

---
+++ snake/VerticalLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int yTop, int yBottom, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yTop; y <= yBottom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+
+    }
+}
